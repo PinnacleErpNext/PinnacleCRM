0f2f9b69a74f0318bacebf1532cd196e45cabe8d
Piyush Shukla <piyushs@mygstcafe.in>
2025-04-07 16:01:40 +0530

    Fix delivery date issue.
    By: Piyush Shukla
    Date: 07-04-2025

--- pinnaclecrm/public/js/sales_order_customization.js
+++ pinnaclecrm/public/js/sales_order_customization.js
@@ -91,9 +91,9 @@ frappe.ui.form.on("Sales Order", {
     pinnaclecrm.utils.applyCustomerGroupFilter(frm, "customer");
   },
   refresh: function (frm) {
-    frm.set_value("delivery_date", 1 - 1 - 2080);
     if (frm.is_new()) {
       frm.set_value("naming_series", "");
+      frm.set_value("delivery_date", 1 - 1 - 2080);
     }
     if (!frm.page.wrapper[0]) return;
 

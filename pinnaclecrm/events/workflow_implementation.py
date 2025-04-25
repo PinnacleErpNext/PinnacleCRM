3f5b00f1bf653e9690e8d5d340894ccc39bee81f
Piyush Shukla <piyushs@mygstcafe.in>
2025-04-25 15:42:54 +0530

    Changes in sales team table.
    Date: 25-04-2025
    By: Piyush Shukla

--- pinnaclecrm/events/workflow_implementation.py
+++ pinnaclecrm/events/workflow_implementation.py
@@ -2,12 +2,14 @@ import frappe
 from erpnext.selling.doctype.sales_order.sales_order import make_sales_invoice
 
 
+# Skip delivery note for sales order.
 def skip_delivery_note(self, method):
     skip_dn = ("SO-G-.FY.-.#.", "SO-GR-.FY.-.#.", "SO-D-.FY.-.#.")
     if self.naming_series in skip_dn:
         self.skip_delivery_note = 1
 
 
+# Mark sales order completed after submit.
 def update_sales_order_status(self, method):
     if (
         self.naming_series in ["SO-G-.FY.-.#.", "SO-GR-.FY.-.#.", "SO-D-.FY.-.#."]
@@ -21,6 +23,7 @@ def update_sales_order_status(self, method):
         frappe.db.commit()
 
 
+# Mark sales order completed when delivery note submitted and mark delivery note completed also if sales order is of free type.
 def mark_so_completed(self, method):
 
     if self.items[0].against_sales_order:
